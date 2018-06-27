31217171594c65997c6ecfa54f3d5703728b8ee5
Bhagyashree Aher <bhagyashree@digitalprizm.net>
2018-06-27 23:42:27 +0530

    updated code

--- registration/config/desktop.py
+++ registration/config/desktop.py
@@ -10,5 +10,29 @@ def get_data():
 			"icon": "octicon octicon-clippy",
 			"type": "module",
 			"label": _("Registration")
-		}
+		},
+		{
+			"module_name": "Events",
+			"_doctype": "Events",
+			"color": "#b512de",
+			"icon": "fa fa-calendar-check-o",
+			"type": "link",
+			"link": "List/Events"
+		},
+		{
+			"module_name": "Participant Data",
+			"_doctype": "Participant Data",
+			"color": "#800000",
+			"icon": "fa fa-file-powerpoint-o",
+			"type": "link",
+			"link": "List/Participant Data"
+		},
+		{
+			"module_name": "Update Participant",
+			"_doctype": "Update Participant",
+			"color": "#b512de",
+			"icon": "fa fa-upload",
+			"type": "link",
+			"link": "List/Update Participant"
+		},
 	]
